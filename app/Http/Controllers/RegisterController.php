36ff6a01025e44dd3778b6dd9d57da47de36f2e5
Paulo <pacecama7006@gmail.com>
2022-08-09 05:53:30 -0500

    Se crea formulario de registro,, así como sus reglas de validación en el controlador register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,9 +7,33 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    /**
+     * Función que nos permite
+     * visualizar la vista register
+     * 
+     * @return registerView
+     */
     public function index()
     {
         return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        // dd('Post');
+        // Accedo a todos los valores
+        // dd($request);
+        // Accedo a propiedades del request
+        // dd($request->get('name'));
+
+        // Validación
+        $this->validate($request, [
+            'name'=> 'required|max:5',
+            'userName' => 'required|unique:users|min:3|max:20',
+            'email' => 'required|unique:users|email|max:60',
+            'password' => 'required',
+            'password_confirmation' => 'required'
+        ]);
+
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
                 <h1 class="text-3xl font-black">Devstagram</h1>
                 <nav class="flex gap-2 items-center">
                     <a href="#" class="font-bold uppercase text-gray-600 text-sm">Login</a>
-                    <a href="/crear-cuenta" class="font-bold uppercase text-gray-600 text-sm">Crear cuenta</a>
+                    <a href="{{ route('register') }} " class="font-bold uppercase text-gray-600 text-sm">Crear cuenta</a>
                 </nav>
             </div>
             {{-- Fin div-container --}}
